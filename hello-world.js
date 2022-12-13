538c179c69fa5d8a352fd4d792879c668be17ce7
adhis-lolo <116695587+adhis-lolo@users.noreply.github.com>
2022-12-13 10:55:02 +0300

    posted  first code hello-world

---
+++ hello-world.js
@@ -0,0 +1,68 @@
+var firstname= "Joyce"
+var secondname="Adhiambo" 
+var password=1234
+
+var newFirstname= prompt(`enter firstname:`)
+var newSecondname= prompt(`enter secondname:`)
+var newpassword=prompt(`enter password:`)
+
+if(newFirstname==firstname)alert(`firstname:correct`)
+if (newSecondname==Secondname)alert(`Secondname :correct`)
+if (password ==newpassword)alert(`Password:correct`)
+
+if (newFirstname!=firstname)alert(`firstname:incorrect`)
+if (newSecondname!=Secondname)alert(`Secondname :incorrect`)
+if (password !=newpassword)alert(`Password:incorrect`)
+VM7502:10 Uncaught ReferenceError: Secondname is not defined
+    at <anonymous>:10:20
+(anonymous) @ VM7502:10
+var firstname= "Joyce"
+var secondname="Adhiambo" 
+var password=1234
+
+var newFirstname= prompt(`enter firstname:`)
+var newSecondname= prompt(`enter secondname:`)
+var newpassword=prompt(`enter password:`)
+
+if(newFirstname==firstname)alert(`firstname:correct`)
+if (newSecondname==secondname)alert(`Secondname :correct`)
+if (password ==newpassword)alert(`Password:correct`)
+
+if (newFirstname!=firstname)alert(`firstname:incorrect`)
+if (newSecondname!=Secondname)alert(`Secondname :incorrect`)
+if (password !=newpassword)alert(`Password:incorrect`)
+VM7520:14 Uncaught ReferenceError: Secondname is not defined
+    at <anonymous>:14:20
+(anonymous) @ VM7520:14
+var firstname= "Joyce"
+var secondname="Adhiambo" 
+var password=1234
+
+var newFirstname= prompt(`enter firstname:`)
+var newSecondname= prompt(`enter secondname:`)
+var newpassword=prompt(`enter password:`)
+
+if(newFirstname==firstname)alert(`firstname:correct`)
+if (newSecondname==secondname)alert(`Secondname :correct`)
+if (password ==newpassword)alert(`Password:correct`)
+
+if (newFirstname!=firstname)alert(`firstname:incorrect`)
+if (newSecondname!=secondname)alert(`Secondname :incorrect`)
+if (password !=newpassword)alert(`Password:incorrect`)
+undefined
+var firstname= "Joyce"
+var secondname="Adhiambo" 
+var password=1234
+
+var newFirstname= prompt(`enter firstname:`)
+var newSecondname= prompt(`enter secondname:`)
+var newpassword=prompt(`enter password:`)
+
+if(newFirstname==firstname)alert(`firstname:correct`)
+if (newSecondname==secondname)alert(`secondname :correct`)
+if (password ==newpassword)alert(`password:correct`)
+
+if (newFirstname!=firstname)alert(`firstname:incorrect`)
+if (newSecondname!=secondname)alert(`secondname :incorrect`)
+if (password !=newpassword)alert(`password:incorrect`)
+undefined
